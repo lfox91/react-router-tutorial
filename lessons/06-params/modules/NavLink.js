ce4c412fdf2fba15d45c3faedd601aad869797b0
Lea Fox <lsfox91@gmail.com>
2016-12-07 11:01:46 -0800

    Update NavLink.js
    I didn't know you could do this very cool but a bit hard to read.

--- lessons/06-params/modules/NavLink.js
+++ lessons/06-params/modules/NavLink.js
@@ -4,6 +4,10 @@ import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
+    {/*
+      The code below is equivalent to:
+      return <Link to={this.props.to} activeClassName="active">{this.props.children}</Link> 
+     */} 
     return <Link {...this.props} activeClassName="active"/>
   }
 })
